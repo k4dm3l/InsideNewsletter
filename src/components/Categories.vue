cc564b95e0270ee51bf2bb1291c007ceeb0a6a6d
Camilo <c.acevedo1993@gmail.com>
2019-09-21 23:12:51 -0500

    Init commit

---
+++ src/components/Categories.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="container-categories container-fluid d-flex p-3">
+    <ul class="categorie-list align-self-start" v-for="(tag, index) in tags" :key="tag.id">
+      <li v-if="index == 0">
+          <a href="/">All</a>
+      </li>
+      <li v-else>
+          <a href="#">{{ tag.name }}</a>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+    import axios from 'axios';
+
+    export default {
+        name: 'navbar-categories',
+        data(){
+            return {
+                url_end_point: `${process.env.VUE_APP_URL}/tags`,
+                tags: []
+            }
+        },
+        methods: {
+            getTags(){
+                const endpoint = this.url_end_point;
+                axios.get(endpoint)
+                    .then(response => {
+                        this.tags = response.data;
+                    })
+                    .catch(err => {
+                        alert(err)
+                    });
+            }
+        },
+        created(){
+            this.getTags();
+        }
+    };
+</script>
+<style scoped>
+    .container-categories {
+        background: #ff5c03;
+        box-shadow: 0 4px 2px -2px gray;        
+    }
+    .categorie-list li {
+        display: inline;
+        margin: 1.3em;
+    }
+
+    li a {
+        color: #fff;
+        text-decoration: none;
+    }
+
+    li a:hover {
+        color: #fff;
+        transition: 0.5s;
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,63 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header/>
+    <div class="container-fluid">
+      <h1 class="mt-5">Noticias reales, elegidas por humanos reales</h1>
+      <p class="mt-4">Noticias, tendencias y enlaces que te ayudarán a mantenerte informado.</p>
+      <div class="container mt-5">
+        <hr>
+      </div>
+      <div class="row m-5">
+        <div class="col-4" v-for="newsletter in newsletters" :key="newsletter.id">
+          <CardNewsletter :card_newsletter_data="newsletter"/>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Header from '@/components/Header.vue';
+import CardNewsletter from '@/components/CardNewsletter.vue';
+import axios from 'axios';
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+    name: 'home',
+    components: {
+      Header,
+      CardNewsletter
+    },
+    data(){
+      return {
+        url_end_point: `${process.env.VUE_APP_URL}/newsletters`,
+        newsletters: []
+      }
+    },
+    methods: {
+      getAllNewsletters(){
+        const endpoint = this.url_end_point;
+
+        axios.get(endpoint)
+          .then(response => {
+            this.newsletters = response.data;
+          })
+          .catch(err => {
+            alert(err);
+          });
+      },
+      seeData(data){
+        console.log(data);
+      }
+    },
+    created(){
+      this.getAllNewsletters();
+    }
 }
 </script>
+<style scoped>
+  p {
+    color: #cccccc;
+    font-size: 1.3em;
+  }
+</style>
